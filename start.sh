ec6666bd7d96e5f731eb31059aed3d6caccc0c19
burkeblazer <burke.blazer@gmail.com>
2018-11-22 09:55:55 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y git vim install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce
+
+git clone https://github.com/docker/compose.git /home/pi/git/compose
+cd /home/pi/git/compose
+git checkout release
