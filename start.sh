89353946656355ccaa802d7ee0ec97a76f1bd095
burkeblazer <burke.blazer@gmail.com>
2018-11-22 10:11:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,7 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get -y install git vim apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+curl -sSL https://get.docker.com | sh
 
 git clone https://github.com/docker/compose.git /home/pi/git/compose
 chown pi:pi -R /home/pi/git/compose
