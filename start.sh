98131727ad701c3a7c7b33c3149de403d82402b0
burkeblazer <burke.blazer@gmail.com>
2018-11-22 10:05:29 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,4 +12,10 @@ apt-get install docker-ce
 git clone https://github.com/docker/compose.git /home/pi/git/compose
 chown pi:pi -R /home/pi/git/compose
 cd /home/pi/git/compose
-git checkout release
+git checkout remotes/origin/release
+docker build -t docker-compose:armhf -f Dockerfile.armhf .
+docker run --rm --entrypoint="script/build/linux-entrypoint" -v $(pwd)/dist:/code/dist -v $(pwd)/.git:/code/.git "docker-compose:armhf"
+cp dist/docker-compose-Linux-armv7l /usr/local/bin/docker-compose
+chown root:root /usr/local/bin/docker-compose
+chmod 0755 /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
