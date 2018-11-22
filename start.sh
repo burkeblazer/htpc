ffb0af2f3c911859aa923b7559762f13e4d960c0
burkeblazer <burke.blazer@gmail.com>
2018-11-22 09:59:08 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get -y git vim install apt-transport-https ca-certificates curl software-properties-common
+apt-get -y install git vim apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
